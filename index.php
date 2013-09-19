ba3b8b7a23b62a9c09983a117e10baf21897f732
drake1004 <drake1004@o2.pl>
2013-09-19 14:13:31 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php 
+
+/**
+ * execute class 
+ */
+
+use library\curl;
+$root_path = dirname(__DIR__);
+
+require_once 'library\curl.php';
+
+if(isset($_GET['keyword'])){
+    
+    $keyword = explode(" ",$_GET['keyword']);
+    $keyword = str_replace(array("'",'"'),"",$keyword[0]);
+    
+    $url = "http://xlab.pl/feed/";
+
+    $curl = new curl($url);
+    $data = $curl->returnData("rss");
+
+    if($data)
+    {
+        $items = $data->channel->item;
+       
+        $allItems = array();
+       
+        foreach($items as $item)
+        {
+            $description = $item->description;
+            preg_match("/$keyword/Ui",$description,$results);
+            if(count($results)>0){
+                $allItems [] = $item->asXML();
+            }
+
+        }
+        foreach($allItems as $itemContent){
+            echo $itemContent;
+        }
+    }
+    
+}
